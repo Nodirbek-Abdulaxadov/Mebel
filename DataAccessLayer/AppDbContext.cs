6c65ed5f73649da174397b3d0298c982219144dd
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2024-01-05 01:16:37 +0500

    uncomment DB eC

--- DataAccessLayer/AppDbContext.cs
+++ DataAccessLayer/AppDbContext.cs
@@ -10,7 +10,7 @@ public class AppDbContext : IdentityDbContext<User>
     public AppDbContext(DbContextOptions<AppDbContext> options) 
         : base(options)
     {
-        //Database.EnsureCreated();
+        Database.EnsureCreated();
     }
 
     public DbSet<Category> Categories { get; set; }
